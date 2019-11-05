d52b6620fa5e7343046e9c0887106b50b945434f
cristian1997 <cristian.vintur@gmail.com>
2019-11-05 08:19:15 +0200

    python script

---
+++ data_preprocessing/script.py
@@ -0,0 +1,257 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from pathlib import Path
+import matplotlib.pyplot as plt
+from tqdm import tqdm_notebook
+import IPython
+import IPython.display
+import PIL
+import librosa
+import librosa.display
+import os
+from fastai import *
+from fastai.vision import *
+from fastai.vision.data import *
+import random
+
+def read_audio(conf, pathname, trim_long_data):
+    y, sr = librosa.load(pathname, sr=conf.sampling_rate)
+    # trim silence
+    if 0 < len(y): # workaround: 0 length causes error
+        y, _ = librosa.effects.trim(y) # trim, top_db=default(60)
+    # make it unified length to conf.samples
+    if len(y) > conf.samples: # long enough
+        if trim_long_data:
+            y = y[0:0+conf.samples]
+    else: # pad blank
+        padding = conf.samples - len(y)    # add padding at both ends
+        offset = padding // 2
+        y = np.pad(y, (offset, conf.samples - len(y) - offset), 'constant')
+    return y
+
+def audio_to_melspectrogram(conf, audio):
+    spectrogram = librosa.feature.melspectrogram(audio,
+                                                 sr=conf.sampling_rate,
+                                                 n_mels=conf.n_mels,
+                                                 hop_length=conf.hop_length,
+                                                 n_fft=conf.n_fft,
+                                                 fmin=conf.fmin,
+                                                 fmax=conf.fmax)
+    spectrogram = librosa.power_to_db(spectrogram)
+    spectrogram = spectrogram.astype(np.float32)
+    return spectrogram
+
+def show_melspectrogram(conf, mels, title='Log-frequency power spectrogram'):
+    librosa.display.specshow(mels, x_axis='time', y_axis='mel',
+                             sr=conf.sampling_rate, hop_length=conf.hop_length,
+                            fmin=conf.fmin, fmax=conf.fmax)
+    plt.colorbar(format='%+2.0f dB')
+    plt.title(title)
+    plt.show()
+
+def read_as_melspectrogram(conf, pathname, trim_long_data, debug_display=False):
+    x = read_audio(conf, pathname, trim_long_data)
+    mels = audio_to_melspectrogram(conf, x)
+    if debug_display:
+        IPython.display.display(IPython.display.Audio(x, rate=conf.sampling_rate))
+        show_melspectrogram(conf, mels)
+    return mels
+
+
+class conf:
+    # Preprocessing settings
+    sampling_rate = 44100
+    duration = 2
+    hop_length = 347*duration # to make time steps 128
+    fmin = 20
+    fmax = sampling_rate // 2
+    n_mels = 128
+    n_fft = n_mels * 20
+    samples = sampling_rate * duration
+
+def mono_to_color(X, mean=None, std=None, norm_max=None, norm_min=None, eps=1e-6):
+    # Stack X as [X,X,X]
+    X = np.stack([X, X, X], axis=-1)
+
+    # Standardize
+    mean = mean or X.mean()
+    std = std or X.std()
+    Xstd = (X - mean) / (std + eps)
+    _min, _max = Xstd.min(), Xstd.max()
+    norm_max = norm_max or _max
+    norm_min = norm_min or _min
+    if (_max - _min) > eps:
+        # Scale to [0, 255]
+        V = Xstd
+        V[V < norm_min] = norm_min
+        V[V > norm_max] = norm_max
+        V = 255 * (V - norm_min) / (norm_max - norm_min)
+        V = V.astype(np.uint8)
+    else:
+        # Just zero
+        V = np.zeros_like(Xstd, dtype=np.uint8)
+    return V
+
+def convert_wav_to_image(df, source, img_dest):
+    X = []
+    for i, row in tqdm_notebook(df.iterrows()):
+        x = read_as_melspectrogram(conf, source/str(row.fname), trim_long_data=False)
+        x_color = mono_to_color(x)
+        X.append(x_color)
+    return X
+
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+print(os.listdir("data"))
+
+# Any results you write to the current directory are saved as output.
+
+DATA = Path('data')
+CSV_TRN_CURATED = DATA/'train_curated.csv'
+CSV_TRN_NOISY = DATA/'train_noisy.csv'
+CSV_SUBMISSION = DATA/'sample_submission.csv'
+TRN_CURATED = DATA/'train_curated'
+TRN_NOISY = DATA/'train_noisy'
+TEST = DATA/'test'
+
+WORK = Path('work')
+IMG_TRN_CURATED = WORK/'image/trn_curated'
+IMG_TRN_NOISY = WORK/'image/train_noisy'
+IMG_TEST = WORK/'image/test'
+for folder in [WORK, IMG_TRN_CURATED, IMG_TRN_NOISY, IMG_TEST]:
+    Path(folder).mkdir(exist_ok=True, parents=True)
+
+df = pd.read_csv(CSV_TRN_CURATED)
+test_df = pd.read_csv(CSV_SUBMISSION)
+
+# example
+x = read_as_melspectrogram(conf, TRN_CURATED/'0006ae4e.wav', trim_long_data=False, debug_display=True)
+
+
+X_train = convert_wav_to_image(df, source=TRN_CURATED, img_dest=IMG_TRN_CURATED)
+X_test = convert_wav_to_image(test_df, source=TEST, img_dest=IMG_TEST)
+
+CUR_X_FILES, CUR_X = list(df.fname.values), X_train
+
+def open_fat2019_image(fn, convert_mode, after_open)->Image:
+    # open
+    idx = CUR_X_FILES.index(fn.split('/')[-1])
+    x = PIL.Image.fromarray(CUR_X[idx])
+    # crop
+    time_dim, base_dim = x.size
+    crop_x = random.randint(0, time_dim - base_dim)
+    x = x.crop([crop_x, 0, crop_x+base_dim, base_dim])
+    # standardize
+    return Image(pil2tensor(x, np.float32).div_(255))
+
+vision.data.open_image = open_fat2019_image
+
+# from official code https://colab.research.google.com/drive/1AgPdhSp7ttY18O3fEoHOQKlt_3HJDLi8#scrollTo=cRCaCIb9oguU
+def _one_sample_positive_class_precisions(scores, truth):
+    """Calculate precisions for each true class for a single sample.
+
+    Args:
+      scores: np.array of (num_classes,) giving the individual classifier scores.
+      truth: np.array of (num_classes,) bools indicating which classes are true.
+
+    Returns:
+      pos_class_indices: np.array of indices of the true classes for this sample.
+      pos_class_precisions: np.array of precisions corresponding to each of those
+        classes.
+    """
+    num_classes = scores.shape[0]
+    pos_class_indices = np.flatnonzero(truth > 0)
+    # Only calculate precisions if there are some true classes.
+    if not len(pos_class_indices):
+        return pos_class_indices, np.zeros(0)
+    # Retrieval list of classes for this sample.
+    retrieved_classes = np.argsort(scores)[::-1]
+    # class_rankings[top_scoring_class_index] == 0 etc.
+    class_rankings = np.zeros(num_classes, dtype=np.int)
+    class_rankings[retrieved_classes] = range(num_classes)
+    # Which of these is a true label?
+    retrieved_class_true = np.zeros(num_classes, dtype=np.bool)
+    retrieved_class_true[class_rankings[pos_class_indices]] = True
+    # Num hits for every truncated retrieval list.
+    retrieved_cumulative_hits = np.cumsum(retrieved_class_true)
+    # Precision of retrieval list truncated at each hit, in order of pos_labels.
+    precision_at_hits = (
+            retrieved_cumulative_hits[class_rankings[pos_class_indices]] /
+            (1 + class_rankings[pos_class_indices].astype(np.float)))
+    return pos_class_indices, precision_at_hits
+
+
+def calculate_per_class_lwlrap(truth, scores):
+    """Calculate label-weighted label-ranking average precision.
+
+    Arguments:
+      truth: np.array of (num_samples, num_classes) giving boolean ground-truth
+        of presence of that class in that sample.
+      scores: np.array of (num_samples, num_classes) giving the classifier-under-
+        test's real-valued score for each class for each sample.
+
+    Returns:
+      per_class_lwlrap: np.array of (num_classes,) giving the lwlrap for each
+        class.
+      weight_per_class: np.array of (num_classes,) giving the prior of each
+        class within the truth labels.  Then the overall unbalanced lwlrap is
+        simply np.sum(per_class_lwlrap * weight_per_class)
+    """
+    assert truth.shape == scores.shape
+    num_samples, num_classes = scores.shape
+    # Space to store a distinct precision value for each class on each sample.
+    # Only the classes that are true for each sample will be filled in.
+    precisions_for_samples_by_classes = np.zeros((num_samples, num_classes))
+    for sample_num in range(num_samples):
+        pos_class_indices, precision_at_hits = (
+            _one_sample_positive_class_precisions(scores[sample_num, :],
+                                                  truth[sample_num, :]))
+        precisions_for_samples_by_classes[sample_num, pos_class_indices] = (
+            precision_at_hits)
+    labels_per_class = np.sum(truth > 0, axis=0)
+    weight_per_class = labels_per_class / float(np.sum(labels_per_class))
+    # Form average of each column, i.e. all the precisions assigned to labels in
+    # a particular class.
+    per_class_lwlrap = (np.sum(precisions_for_samples_by_classes, axis=0) /
+                        np.maximum(1, labels_per_class))
+    # overall_lwlrap = simple average of all the actual per-class, per-sample precisions
+    #                = np.sum(precisions_for_samples_by_classes) / np.sum(precisions_for_samples_by_classes > 0)
+    #           also = weighted mean of per-class lwlraps, weighted by class label prior across samples
+    #                = np.sum(per_class_lwlrap * weight_per_class)
+    return per_class_lwlrap, weight_per_class
+
+
+# Wrapper for fast.ai library
+def lwlrap(scores, truth, **kwargs):
+    score, weight = calculate_per_class_lwlrap(to_np(truth), to_np(scores))
+    return torch.Tensor([(score * weight).sum()])
+
+tfms = get_transforms(do_flip=True, max_rotate=0, max_lighting=0.1, max_zoom=0, max_warp=0.)
+src = (ImageList.from_csv(WORK/'image', Path('../../')/CSV_TRN_CURATED, folder='trn_curated')
+       .split_by_rand_pct(0.2)
+       .label_from_df(label_delim=',')
+)
+data = (src.transform(tfms, size=128)
+        .databunch(bs=64).normalize(imagenet_stats)
+)
+
+data.show_batch(3)
+
+learn = cnn_learner(data, models.resnet18, pretrained=False, metrics=[lwlrap])
+learn.unfreeze()
+
+learn.lr_find(); learn.recorder.plot()
+
+learn.fit_one_cycle(5, 1e-1)
+learn.fit_one_cycle(10, 1e-2)
+
+learn.lr_find(); learn.recorder.plot()
+
+learn.save('fat2019_fastai_cnn2d_stage-2')
+learn.export()
