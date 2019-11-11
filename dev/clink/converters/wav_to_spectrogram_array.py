1607bc3ec5d726f15ad825043ccba33e3c7eddf0
cristian1997 <cristian.vintur@gmail.com>
2019-11-11 21:52:09 +0200

    aspects 1.0

--- dev/clink/converters/wav_to_spectrogram_array.py
+++ dev/clink/converters/wav_to_spectrogram_array.py
@@ -1,8 +1,23 @@
+import aspectlib
+
 from dev.clink.converters.converter import Converter
 
+@aspectlib.Aspect(bind = True)
+def log_call(cutpoint, *args, **kargs):
+	with open("calls.txt", "a") as out_file:
+		out_file.write(str(cutpoint.__name__) + "\n")
+	yield
+
+@aspectlib.Aspect()
+def validate_extension(*args, **kargs):
+	sound_path = kargs['sound_path']
+	assert sound_path[-4:] == ".wav"
+	yield
 
 class WavToSpectrogramConverter(Converter):
 
+    @log_call
+    @validate_extension
     def convert_wav(self, sound_path):
         image = self.audio_expert.read_as_melspectrogram(sound_path, trim_long_data=False)
         return self.image_expert.mono_to_color(image)
