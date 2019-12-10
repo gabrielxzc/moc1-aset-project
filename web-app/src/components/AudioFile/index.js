a7f22acf95af20ee4b8030bbe032eff1b7f61d4b
Gabriel-Codrin Cojocaru <codrin.cojocaru@structuralmanagement.ro>
2019-12-10 07:49:40 +0200

    only 1 audio file playing, pause button added

--- web-app/src/components/AudioFile/index.js
+++ web-app/src/components/AudioFile/index.js
@@ -1,45 +1,50 @@
 import React from "react";
 
 import "./_audio-file.scss";
+import { ALL_AUDIO_TAGS } from "../../commonlib/audio-tags";
 
-function AudioFile() {
+const AudioFile = props => {
+  const tags = props.tags
+    ? props.tags
+    : [ALL_AUDIO_TAGS[2], ALL_AUDIO_TAGS[5], ALL_AUDIO_TAGS[8]];
+
   return (
-    <div className="card">
+    <div className="card audio-file">
       <div className="card-content">
         <div className="media">
           <div className="media-left">
             <figure className="image is-48x48">
               <span className="icon is-large" style={{ cursor: "pointer" }}>
-                <i className="fas fa-2x fa-play-circle"></i>
+                {props.isPlaying ? (
+                  <i className="fas fa-2x fa-pause-circle" />
+                ) : (
+                  <i className="fas fa-2x fa-play-circle" />
+                )}
               </span>
             </figure>
           </div>
-          <div className="media-content" style={{ height: "50px" }}>
+          <div className="media-content">
             {[...Array(40).keys()].map(index => (
-              <div key={index} className="bar" />
+              <div
+                key={index}
+                className={`bar ${props.isPlaying ? "bar--animated" : ""}`}
+              />
             ))}
           </div>
         </div>
 
-        <div className="content">
+        <div className="content audio-file-tags">
           <div className="tags are-small">
-            <span className="tag is-success">
-              Chirp
-              <button className="delete is-small"></button>
-            </span>
-            <span className="tag is-success">
-              Fill
-              <button className="delete is-small"></button>
-            </span>
-            <span className="tag is-success">
-              Chink and clink
-              <button className="delete is-small"></button>
-            </span>
+            {tags.map((tag, index) => (
+              <span key={index} className="tag is-success">
+                {tag}
+              </span>
+            ))}
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default AudioFile;
--- web-app/src/components/AudioFiles/index.js
+++ web-app/src/components/AudioFiles/index.js
@@ -1,23 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import queryString from "query-string";
+
 import AudioFile from "../AudioFile";
 
-function AudioFiles() {
-  return (
-    <div className="columns is-centered">
-      <div className="column is-one-fifth">
-        <AudioFile />
-      </div>
-      <div className="column is-one-fifth">
-        <AudioFile />
-      </div>
-      <div className="column is-one-fifth">
-        <AudioFile />
-      </div>
-      <div className="column is-one-fifth">
-        <AudioFile />
-      </div>
-    </div>
-  );
-}
+const AudioFiles = props => {
+  useEffect(() => {
+    console.log(queryString.parse(props.location.search));
+  }, [props.location.search]);
 
-export default AudioFiles;
+  return (
+    <>
+      <div className="columns is-centered">
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+        <div className="column is-one-fifth">
+          <AudioFile isPlaying />
+        </div>
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+      </div>
+      <div className="columns is-centered">
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+      </div>
+      <div className="columns is-centered">
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+        <div className="column is-one-fifth">
+          <AudioFile />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default withRouter(AudioFiles);
