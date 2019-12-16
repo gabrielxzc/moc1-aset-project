eb437b09d2b75d053e55c4f67045cb3ee0e0ed1b
Madalina Alina Racovita <Madalina.Racovita@endava.com>
2019-12-16 22:59:19 +0200

    Started to implement the entities, added requirements

---
+++ rest-api/orm/db_connection_manager.py
@@ -0,0 +1,54 @@
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_batch_inserts import enable_batch_inserting
+from sqlalchemy_utils import database_exists, create_database
+
+from orm.entities import *
+from utils.constants import URL_TO_DATABASE
+
+warnings.filterwarnings("ignore")
+
+
+class DbConnection:
+
+    def __init__(self):
+        pass
+
+    def make_connection(self):
+        url = URL_TO_DATABASE
+        if not database_exists(url):
+            create_database(url)
+
+        # configs and instances
+        engine = sqlalchemy.create_engine(URL_TO_DATABASE)
+        return engine
+
+    def create_tables(self, engine):
+        Session = sessionmaker(bind=engine)
+
+        # create a Session
+        session = Session()
+        enable_batch_inserting(session)
+
+        # create all tables, if not existent already
+        Base.metadata.create_all(engine)
+        return session
+
+
+def try_simple_operation_in_database():
+    dbConnection = DbConnection()
+    engine_db_connection = dbConnection.make_connection()
+
+    session = dbConnection.create_tables(engine=engine_db_connection)
+    session.add(Label(label_name='Bark'))
+    session.commit()
+
+    labels_elem = session.query(Label).all()
+    for label in labels_elem:
+        print(label.label_name)
+
+    print("Inserted in audio-tagging Database.")
+
+
+if __name__ == "__main__":
+    try_simple_operation_in_database()
---
+++ rest-api/utils/constants.py
@@ -0,0 +1,4 @@
+import os
+from redundancydetection.config import USER_DB, PASSWORD_DB, HOST_DB, DATABASE_NAME
+
+URL_TO_DATABASE = 'postgresql://' + USER_DB + ':' + PASSWORD_DB + '@' + HOST_DB + '/' + DATABASE_NAME
