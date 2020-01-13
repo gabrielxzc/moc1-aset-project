4cea3057a1ecb4a0306b0a98308a923d5edef7c2
Cristian Vintur <cristi@pop-os.localdomain>
2020-01-13 22:22:20 +0200

    changed request from get to post

--- rest-api/controllers/tagger_controller.py
+++ rest-api/controllers/tagger_controller.py
@@ -1,19 +1,10 @@
-from flask_restful import Resource, reqparse
-
-from ioc.container import Services
-from service.query_service import IQueryService
+from flask_restful import Resource, request
 
 
 class TaggerController(Resource):
-    def __init__(self):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument("file_str", required=True, type=str, location="args")
+    def post(self):
+        print(request.data)
 
-    def get(self):
-        # TODO: extract magic strings
-        args = self.parser.parse_args()
-        query = args["file_str"]
+        tags = ['bark', 'chainsaw']
 
-        metadata = ['bark', 'chainsaw']
-
-        return metadata
+        return tags
--- rest-api/tagger.py
+++ rest-api/tagger.py
@@ -1,4 +1,12 @@
 from algorithm.submission_tagger import tag_sound
 
+from base64 import b64encode, decodebytes
+
 if __name__ == "__main__":
-    print(tag_sound('0a9bebde.wav'))
+    filename = "0a9bebde.wav"
+
+    encoded_string = b64encode(bytes(open(filename, "r").read(), "utf-8"))
+    print(encoded_string)
+    tmp_image = open("tmp_image.wav", "w")
+    tmp_image.write(decodebytes(encoded_string))
+    print(tag_sound("tmp_image.wav"))
